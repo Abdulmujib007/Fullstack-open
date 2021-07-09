4d99d360fb44f6de932dfdb2fea515dd2b544d9b
Abdulmujib <asiyanbiakorede@gmail.com>
2021-07-09 16:35:22 +0100

    courseinfo corrections

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
+  };
 
   return (
     <div>
@@ -29,7 +29,20 @@ const App = () => {
         name2={course.parts[1].name}
         name3={course.parts[2].name}
         firstExercise={course.parts[0].exercises}
-       secondExercise={course.parts[1].exercises}
+        secondExercise={course.parts[1].exercises}
+        thirdExercise={course.parts[2].exercises}
+      />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      /><Header name={course.name} />
+      <Content
+        name1={course.parts[0].name}
+        name2={course.parts[1].name}
+        name3={course.parts[2].name}
+        firstExercise={course.parts[0].exercises}
+        secondExercise={course.parts[1].exercises}
         thirdExercise={course.parts[2].exercises}
       />
       <Total
