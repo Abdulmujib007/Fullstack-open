a6dc0a2b9b6332f6e24d1c13efb240d1c7646745
Abdulmujib <asiyanbiakorede@gmail.com>
2021-07-13 23:53:33 +0100

    feat:anecdote

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import Button from './components/Button.jsx'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [allVotes,setAllvotes] = useState(Array(7).fill(0))
+
+  const handleSelectedClick = () => setSelected(selected < anecdotes.length-1 ? selected +1 : 0)
+
+  const handleVoteclick = () => {
+    const newVote = [...allVotes]
+    newVote[selected] += 1
+    setAllvotes(newVote)
+  }
+  const highestVote = Math.max(...allVotes)
+  const indexOfhighestVote = allVotes.indexOf(highestVote)
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br/>
+      <div>has  {allVotes[selected]} vote</div>
+      <br/>
+      <Button text = 'next anecdote' handleClick = {handleSelectedClick}/>
+      <Button text = 'vote' handleClick ={handleVoteclick} />
+      <h1>Anecdote with most count</h1>
+      <>
+       {
+         highestVote === 0 
+         ? 
+         <span>No votes have beeen taken yet</span>
+         :
+          <>
+          <p>{anecdotes[indexOfhighestVote]}</p>
+          <span>has {highestVote} vote</span>
+          </>
+       }
+        </>
+       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
