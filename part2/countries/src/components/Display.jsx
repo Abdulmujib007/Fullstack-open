3b58abebb9ce08c97c9c9ac30eeec77cd5be8d7b
Abdulmujib <asiyanbiakorede@gmail.com>
2021-07-24 13:19:01 +0100

    feat:countries completed

--- part2/countries/src/components/Display.jsx
+++ part2/countries/src/components/Display.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Button from "./Button";
 import axios from 'axios'
 
 const Display = ({ displayedCountry }) => {
-  const [weatherData , setWeatherData] = useState({})
+  const [weatherData , setWeatherData] = useState([])
   const [imgs, setImages] = useState(null);
 
-  if(displayedCountry.length ===1) {
+
+  useEffect(() => {
+    if (displayedCountry.length ===1){
+
     const namesCount = displayedCountry[0].name
     console.log(namesCount)
-    axios.get(`http://api.weatherstack.com/current?access_key=70cfcbe75a6724eb1499fa78f8ecf6e1&query=${namesCount}`)
-    .then(({data}) =>  setWeatherData(data))
+    console.log("one country")   
+    axios.get(`http://api.weatherstack.com/current?access_key=7ac8f956d8a79ea57845f0bc79c125a6&query=${namesCount}`)
+    .then(({data}) =>  setWeatherData(weatherData.concat(data)))
     .catch( err =>console.error(err))
-     
-  }
+    }
+    setWeatherData([])
+
+    
+  }, [displayedCountry])
+
 
   const handleShow = (displayedCountryIndex) => {
     if(imgs !== null) {
-      axios.get(`http://api.weatherstack.com/current?access_key=70cfcbe75a6724eb1499fa78f8ecf6e1&query=${displayedCountry[imgs].name}`)
-      .then(({data}) => setWeatherData(data) )
+
+      axios.get(`http://api.weatherstack.com/current?access_key=7ac8f956d8a79ea57845f0bc79c125a6&query=${displayedCountry[imgs].name}`)
+      .then(({data}) => setWeatherData(weatherData.concat(data)) )
       .catch(err => console.error(err))
     }
+    setWeatherData([])
+
     displayedCountryIndex !== imgs
       ? setImages(displayedCountryIndex)
       : setImages(null);
@@ -33,7 +44,7 @@ const Display = ({ displayedCountry }) => {
         <p>Too many matches,specify another filter</p>
       ) : displayedCountry.length === 1 ? (
         
-                displayedCountry.map(
+          displayedCountry.map(
           ({ name, flag, languages, population, capital }) => (
             <>
               <h3 key={`header`}>{name}</h3>
@@ -47,13 +58,15 @@ const Display = ({ displayedCountry }) => {
                 ))}
               </ul>
               <img style={{ width: "10rem" }} key={`img`} src={flag} />
+              {/* {JSON.stringify(weatherData, null, 2)} */}
+              {/* {JSON.stringify({length:weatherData}, null, 2)} */}
               {
                 weatherData.length && (
                   <div>
-                      <p key={`weather${imgs}1`}>Weather in {weatherData.location.name}</p>
-                      <p key={`weather${imgs}2`}>temperature:{weatherData.current.temperature}</p>
-                      <img key={`weather${imgs}3`} style={{width:'5rem'}} src={weatherData.current.weather_icons[0]}/>
-                      <p key={`weather${imgs}4`}>wind:{weatherData.current.wind_speed}mph direction{weatherData.current.wind_dir}</p>
+                      <p key={`weather${imgs}1`}>Weather in {weatherData[0].location.name}</p>
+                      <p key={`weather${imgs}2`}>temperature:{weatherData[0].current.temperature}</p>
+                      <img key={`weather${imgs}3`} style={{width:'5rem'}} src={weatherData[0].current.weather_icons[0]}/>
+                      <p key={`weather${imgs}4`}>wind:{weatherData[0].current.wind_speed}mph direction{weatherData [0].current.wind_dir}</p>
                   </div>
                 )
               }
@@ -95,10 +108,10 @@ const Display = ({ displayedCountry }) => {
                   weatherData.length  ? (
                     
                     <div>
-                      <p key={`weather${imgs}0`}>Weather in {weatherData.location.name}</p>
-                      <p key={`weather${imgs}1`}>temperature:{weatherData.current.temperature}</p>
-                      <img key={`weather${imgs}2`} style={{width:'5rem'}} src={weatherData.current.weather_icons[0]}/>
-                      <p key={`weather${imgs}3`}>wind:{weatherData.current.wind_speed}mph direction{weatherData.current.wind_dir}</p>
+                      <p key={`weather${imgs}0`}>Weather in {weatherData[0].location.name}</p>
+                      <p key={`weather${imgs}1`}>temperature:{weatherData[0].current.temperature}</p>
+                      <img key={`weather${imgs}2`} style={{width:'5rem'}} src={weatherData[0].current.weather_icons[0]}/>
+                      <p key={`weather${imgs}3`}>wind:{weatherData[0].current.wind_speed}mph direction {weatherData[0].current.wind_dir}</p>
                     </div>
                   )
                   : ''
